df0f19c79bb97fb8984da36a779975380f0ca5f3
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2025-11-10 14:51:37 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. TAMBAHKAN INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') { // <-- 2. TAMBAHKAN KONDISI INI
+            URL::forceScheme('https'); // <-- 3. TAMBAHKAN BARIS INI
+        }
     }
 }
